65baeccf559eb988835212a869bf9a4f88e340a5
Huy Ly <45591413+huylg@users.noreply.github.com>
2026-07-07 19:05:14 +0700

    Add: valid-sudoku - submission-38

---
+++ Data Structures & Algorithms/valid-sudoku/submission-38.py
@@ -0,0 +1,48 @@
+class Solution:
+    def isDuplicated(self, nums: [str]) -> bool:
+        s = set()
+
+        for num in nums:
+            if num != '.' and num in s:
+                return True
+            else:
+                s.add(num)
+
+        return False
+
+
+
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        l = 9
+        
+        for i in range(0,l):
+            row = board[i]
+            col = [ row[i] for row in board ]
+
+
+            matrix = []
+
+            for j in range(0,9):
+                x = int(j / 3) + (i%3) *3
+                y = (j % 3) + int(i/3) * 3
+                value = board[x][y]
+                matrix.append(value)
+
+
+            if self.isDuplicated(row) or self.isDuplicated(col) or self.isDuplicated(matrix):
+                return False
+    
+
+        return True
+
+            
+
+
+
+
+
+            
+
+        return true
+
+            
